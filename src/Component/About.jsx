9f8351840540f786821ee4d5be9c3386d8d92125
pratyaksh177 <pratyakshraj17@gmail.com>
2024-08-31 12:32:50 +0530

    project updated

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,16 +1,8 @@
 const About = ({reference}) => {
     return (
       <div ref={reference} id="about" className="text-lg ">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
-        unchanged. 
-        <p>It was popularised in the 1960s with the release of Letraset
-        sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.</p>
+      Hello, I am Pratyaksh Raj, a MERN Stack Developer with experience in building dynamic web applications. In addition to my development skills, I have created a machine learning-based real estate project that showcases my ability to integrate advanced technologies into practical solutions. 
+        <p>Beyond coding, I excel in public speaking and possess strong leadership skills, honed through my active involvement in various student organizations and events. My passion for technology and effective communication drives me to deliver impactful solutions while inspiring and guiding others.</p>
       </div>
     );
   };
--- src/Component/ProjectInfo.jsx
+++ src/Component/ProjectInfo.jsx
@@ -1,35 +1,39 @@
  import img from "./../Img/Screenshot from 2023-10-13 09-23-24.png"
+ import bl from "./../Img/blender.png"
+ import rs from "./../Img/realestate.jpeg"
+ import td from "./../Img/todo.avif"
+ import fla from "./../Img/flappybird.webp"
 const ProjectInfo = [
 
     {
         id: 1,
-        image: img,
-        name: "passbook",
+        image: rs,
+        name: "ML Real-Estate Price Prediction Model",
         subTitle: "For internship",
-        text: "I made this within 5 hour and submitted to my mentor.This Mini Project is a great use of react that include authentication and many other features",
-        techstack: [ "React", "Mongodb", "Authentication", "HTML"],
-        link : "",
+        text: "Developed a machine learning model for real estate price prediction using regression algorithms. The project included data collection, preprocessing, and visualization with Matplotlib to evaluate model performance.",
+        techstack: [ "Python", "NumPy", "Pandas", "Scikit-learn", "Matplotlib"],
+        link : "https://github.com/pratyaksh177/ML-Project",
         gitHub: ""       
 
 
     },
     {
         id: 2,
-        image: img,
-        name: "passbook",
-        text: "I made this within 5 hour and submitted to my mentor.This Mini Project is a great use of react that include authentication and many other features",
-        techstack: [ "React", "Mongodb", "Authentication", "HTML"],
-        link : "",
+        image: fla,
+        name: "Flappy Bird Game ",
+        text: "Developed a web-based Flappy Bird game that mimics the classic arcade-style gameplay. This project involved designing the game interface using HTML and CSS, and implementing game mechanics such as gravity, collision detection, and scoring system using JavaScript.",
+        techstack: [ "HTML", "CSS", "JavaScript"],
+        link : "https://pratyaksh177.github.io/Flappy-Bird/",
         gitHub: ""       
 
 
     },
     {
         id: 3,
-        image: img,
-        name: "passbook",
-        text: "I made this within 5 hour and submitted to my mentor.This Mini Project is a great use of react that include authentication and many other features",
-        techstack: [ "React", "Mongodb", "Authentication", "HTML"],
+        image: bl,
+        name: "Open Source Contribution",
+        text: " Contributed to the Blender open-source project by addressing and resolving a ”good first issue” in the codebase. This contribution involved understanding Blender’s code structure, implementing a solution in C++, and collaborating with the community to ensure the contribution met project standards.",
+        techstack: [ "Cpp" ],
         link : "",
         gitHub: ""       
 
@@ -37,11 +41,23 @@ const ProjectInfo = [
     },
     {
         id: 4,
+        image: td,
+        name: "Full-Stack Todo App",
+        text: "An ongoing project aimed at creating a full-stack Todo application. The frontend is developed using HTML, CSS, and JavaScript, while the backend leverages Node.js and Express to handle server-side logic and manage data persistence.",
+        techstack: [ "React", "Mongodb", "Authentication", "HTML"],
+        link : "https://github.com/pratyaksh177/Todo-app",
+        gitHub: ""       
+
+
+    },
+
+    {
+        id: 5,
         image: img,
         name: "passbook",
         text: "I made this within 5 hour and submitted to my mentor.This Mini Project is a great use of react that include authentication and many other features",
         techstack: [ "React", "Mongodb", "Authentication", "HTML"],
-        link : "",
+        link : "https://projects.blender.org/blender/blender/pulls/105193",
         gitHub: ""       
 
 
